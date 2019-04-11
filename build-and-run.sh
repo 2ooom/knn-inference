9b2caa3f6c2659c0ef52fed4c1a5a4ecb6f0dc0d
Dmitry Parfenchik <d.parfenchik@criteo.com>
2019-04-11 23:39:25 +0200

    Tf native benchmark update

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-/usr/local/opt/llvm/bin/clang++ src/main/cpp/hello.cpp -ltensorflow -std=c++11 -openmp -march=native -fpic -fopenmp -w -ftree-vectorize -o hello
+#/usr/local/opt/llvm/bin/clang++ src/main/cpp/hello.cpp -ltensorflow -std=c++11 -openmp -march=native -fpic -fopenmp -w -ftree-vectorize -o hello
+/usr/local/opt/llvm/bin/clang++ src/main/cpp/hello.cpp -ltensorflow -march=x86-64 -m64 -O3 -std=c++11 -Wl,-rpath,@loader_path/. -Wall -fPIC -undefined dynamic_lookup -o hello
 ./hello
--- src/main/cpp/hello.cpp
+++ src/main/cpp/hello.cpp
@@ -15,7 +15,11 @@ TF_Buffer *read_file(const char *path);
 TF_Graph *read_graph(const char *path, TF_Status *status);
 Index<float> *create_euclidean(int dimension, const size_t nbItems, const size_t M, const size_t efConstruction, const size_t random_seed);
 Index<float> *load_euclidean(int dimension);
-float* get_model_input(int dimension, int extra_dimension, int nb_embeddings, Index<float> * index, int nb_items);
+float* get_model_input(int dimension, int extra_dimension, Index<float> * index, int ids[], float* result);
+void set_random_ids(int* ids, int len, int max_id);
+std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::high_resolution_clock::duration> now();
+float get_elepased_microseconds(std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::high_resolution_clock::duration> start);
+float get_elepased_seconds(std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::high_resolution_clock::duration> start);
 
 const float RAND_MAX_FLOAT = (float)(RAND_MAX);
 
@@ -48,6 +52,7 @@ int main() {
     int extra_dimension = 5;
     int dimension = embeddings_dimension + extra_dimension;
     int M = 16;
+    int k = 20;
     int efConstruction = 200;
     int random_seed = 42;
 
@@ -68,7 +73,7 @@ int main() {
     TF_Output output_opout = {output_op, 0};
     outputs.push_back(output_opout);
 
-    //Index<float> *index = create_euclidean(embeddings_dimension, nb_items, M, efConstruction, random_seed);
+    //Index<float> *index = create_euclidean(embeddings_dimension, nb_items, M, efConstruction, rand());
     Index<float> *index = load_euclidean(embeddings_dimension);
 
     // Creating session
@@ -77,7 +82,8 @@ int main() {
     assert(TF_GetCode(status) == TF_OK);
 
     // Create variables to store the size of the input and output variables
-    const int num_bytes_in = dimension * nb_embeddings * sizeof(float);
+    const int input_size = dimension * nb_embeddings;
+    const int num_bytes_in = input_size * sizeof(float);
     const int num_bytes_out = dimension * sizeof(float);
 
     int64_t in_dims[] = {nb_embeddings, dimension};
@@ -85,32 +91,64 @@ int main() {
 
     for (int i = 0; i < nb_iterations; i++) {
         // Creating input
-        auto start = std::chrono::high_resolution_clock::now();
-        float *values = get_model_input(embeddings_dimension, extra_dimension, nb_embeddings, index, nb_items);
-        TF_Tensor* input = TF_NewTensor(TF_FLOAT, in_dims, 2 /*in_dims.length*/, values, num_bytes_in, free_tensor, 0);
+        std::vector<int> ids(nb_embeddings);
+        set_random_ids(ids.data(), nb_embeddings, nb_items);
+        auto start = now();
+        std::vector<float> values(input_size);
+        float* values_data = values.data();
+        get_model_input(embeddings_dimension, extra_dimension, index, ids.data(), values_data);
+        float get_model_input_microseconds = get_elepased_microseconds(start);
+
+        start = now();
+        TF_Tensor* input = TF_NewTensor(TF_FLOAT, in_dims, 2/*len(in_dims)*/, values_data, num_bytes_in, free_tensor, 0);
         std::vector<TF_Tensor*> input_values;
         input_values.push_back(input);
+        float input_tensor_microseconds = get_elepased_microseconds(start);
         //std::cout << "Input data info: " << TF_Dim(input, 0) << "x"<< TF_Dim(input, 1) << "\n";
 
         // Allocating output
-        TF_Tensor* output_value = TF_AllocateTensor(TF_FLOAT, out_dims, 1 /*out_dims.length*/, num_bytes_out);
+        start = now();
+        TF_Tensor* output_value = TF_AllocateTensor(TF_FLOAT, out_dims, 1/*len(out_dims)*/, num_bytes_out);
         std::vector<TF_Tensor*> output_values(outputs.size(), nullptr);
         output_values.push_back(output_value);
+        float output_tensor_microseconds = get_elepased_microseconds(start);
         //std::cout << "Output info: " << TF_Dim(output_value, 0) << "\n";
 
         // Running graph
+        start = now();
         TF_SessionRun(session, nullptr,
                         &inputs[0], &input_values[0], inputs.size(),
                         &outputs[0], &output_values[0], outputs.size(),
                         nullptr, 0, nullptr, status);
-        
+        float run_session_microseconds = get_elepased_microseconds(start);
+
+        start = now();
         float* out_vals = static_cast<float*>(TF_TensorData(output_values[0]));
+        std::vector<float> query(embeddings_dimension);
+        for(int i = 0; i < embeddings_dimension; i++) {
+            query[i] = *out_vals++;
+        }
         /*for (int i = 0; i < dimension; ++i) {
             std::cout << "Output values info: " << *out_vals++ << "\n";
         }*/
-        auto elapsed = std::chrono::high_resolution_clock::now() - start;
-        float microseconds = (float)std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-        fprintf(stdout, "%.2f\n", microseconds);
+        float read_output_microseconds = get_elepased_microseconds(start);
+        
+        start = now();
+        std::vector<float> distances(k);
+        std::vector<size_t> items(k);
+        index->knnQuery(query.data(), items.data(), distances.data(), k);
+        float knn_microseconds = get_elepased_microseconds(start);
+        /*for(int i = 0; i < k; i++) {
+            std::cout << items[i] << " -> " << distances[i] << "\n";
+        }*/
+        float total_elapsed = get_model_input_microseconds + input_tensor_microseconds + output_tensor_microseconds + run_session_microseconds + knn_microseconds;
+        std::cout <<
+            get_model_input_microseconds << ", " <<
+            input_tensor_microseconds << ", " <<
+            output_tensor_microseconds << ", " <<
+            run_session_microseconds << ", " <<
+            knn_microseconds << ", " <<
+            total_elapsed << "\n";
     }
 
     fprintf(stdout, "Successfully run session\n");
@@ -155,7 +193,7 @@ TF_Buffer *read_file(const char *path) {
 
 Index<float> *create_euclidean(int dimension, const size_t nbItems, const size_t M, const size_t efConstruction, const size_t random_seed) {
     fprintf(stdout, "Building index\n");
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = now();
     hnswlib::SpaceInterface<float> *space = new hnswlib::L2Space(dimension);
     bool normalize = false;
     Index<float> *index = new Index<float>(space, dimension, normalize);
@@ -167,9 +205,7 @@ Index<float> *create_euclidean(int dimension, const size_t nbItems, const size_t
         }
         index->addItem(embedding.data(), i);
     }
-    auto elapsed = std::chrono::high_resolution_clock::now() - start;
-    float seconds = (float)std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count()/1000000.0f;
-    fprintf(stdout, "Built index of %d items in: %.2fs\n", nbItems, seconds);
+    fprintf(stdout, "Built index of %d items in: %.2fs\n", nbItems, get_elepased_seconds(start));
     index->saveIndex(path_to_index);
     std::cout << "Saved index to " << path_to_index << "\n";
     return index;
@@ -182,29 +218,46 @@ Index<float> *load_euclidean(int dimension) {
     Index<float> *index = new Index<float>(space, dimension, normalize);
     auto start = std::chrono::high_resolution_clock::now();
     index->loadIndex(path_to_index);
-    auto elapsed = std::chrono::high_resolution_clock::now() - start;
-    float seconds = (float)std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count()/1000000.0f;
-    fprintf(stdout, "Loaded index of %d items in: %.2fs\n", index->appr_alg->cur_element_count, seconds);
+    fprintf(stdout, "Loaded index of %d items in: %.2fs\n", index->appr_alg->cur_element_count, get_elepased_seconds(start));
     return index;
 }
 
-float* get_model_input(int dimension, int extra_dimension, int rows, Index<float> * index, int nb_items) {
+float* get_model_input(int dimension, int extra_dimension, Index<float> * index, int ids[], float* result) {
     int cols = dimension + extra_dimension;
+    int rows = sizeof(ids);
     int len = rows * cols;
     // We need to allocate contiguous memory block to pass into TF_Tensor
-    float* result = new float[len];
     for(int i = 0; i < rows; i++) {
-        int id = get_random_id(nb_items);
+        int id = ids[i];//get_random_id(nb_items);
         int row_shift = i*cols;
         std::vector<float> embedding = index->appr_alg->template getDataByLabel<float>(id);
         for(int j = 0; j < dimension; j++) {
             result[row_shift + j] = embedding[j];
         }
         for(int j = dimension; j < cols; j++) {
-            result[row_shift + j] = j * i;
+            result[row_shift + j] = result[row_shift + j - dimension];
         }
         //std::cout << "Input "<< i << " [" << id << "] First = " << result[row_shift] << "; Last = " << result[row_shift + cols - 1] << " index = " << row_shift + cols - 1 <<"\n";
     }
     //std::cout << "Input first="<< result[0] << " Last=" << result[len - 1] << "\n";
     return result;
+}
+
+void set_random_ids(int* ids, int len, int max_id) {
+    for (int i = 0; i < len; i++) {
+        ids[i] = get_random_id(max_id);
+    }
+}
+
+std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::high_resolution_clock::duration> now() {
+    return std::chrono::high_resolution_clock::now();
+}
+
+float get_elepased_microseconds(std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::high_resolution_clock::duration> start) {
+    auto elapsed = std::chrono::high_resolution_clock::now() - start;
+    return (float)std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+}
+
+float get_elepased_seconds(std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::high_resolution_clock::duration> start) {
+    return get_elepased_microseconds(start)/1000000.0f;
 }
